3413c2a91eb5012f05248a8921022db838fc3df8
Ayush <ayushdesai1000@gmail.com>
2024-03-20 13:46:46 +0530

    Fetch All Posts working

--- server/controller/PostController.js
+++ server/controller/PostController.js
@@ -1,22 +1,20 @@
-const PostModel = require("../model/PostModel")
+const PostModel = require("../model/PostModel");
 
-module.exports.createPost = async (req,res)=>{
-    const {title, content, posterUserId,posterUsername } = req.body;
+module.exports.createPost = async (req, res) => {
+    const { title, content, posterUserId, posterUsername } = req.body;
     const newPost = new PostModel({
-    title,
-    content,
-    posterUserId,
-    posterUsername
-    })
-    try{
+        title,
+        content,
+        posterUserId,
+        posterUsername
+    });
+    try {
         const result = await newPost.save();
-        res.status(200).json(result)
+        res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json(error);
     }
-    catch(error){
-        res.status(500).json(error)
-    }
-
-}
+};
 
 module.exports.findPost = async (req, res) => {
     const { postId } = req.params;
@@ -30,15 +28,23 @@ module.exports.findPost = async (req, res) => {
     } catch (error) {
         res.status(500).json(error);
     }
-}
+};
 
-module.exports.deletePost = async(req,res)=>{
-    const{postId} = req.params
-    try{
-        const del = await PostModel.findByIdAndDelete(postId);
-        res.status(200).json("DELETED")
+module.exports.deletePost = async (req, res) => {
+    const { postId } = req.params;
+    try {
+        await PostModel.findByIdAndDelete(postId);
+        res.status(200).json("DELETED");
+    } catch (error) {
+        res.status(500).json(error);
     }
-    catch(error){
-        res.status(500).json(error)
+};
+
+module.exports.fetchPosts = async (req, res) => {
+    try {
+        const allPosts = await PostModel.find();
+        res.status(200).json(allPosts);
+    } catch (error) {
+        res.status(500).json(error);
     }
-}
+};
--- server/routes/PostRoute.js
+++ server/routes/PostRoute.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const {createPost, findPost, deletePost} = require("../controller/PostController");
+const {createPost, findPost, deletePost, fetchPosts} = require("../controller/PostController");
 
 const router = express.Router();
 
 router.post("/", createPost);
+router.get("/all", fetchPosts)
 router.get("/:postId", findPost)
 router.post("/delete", deletePost)
 module.exports = router;
