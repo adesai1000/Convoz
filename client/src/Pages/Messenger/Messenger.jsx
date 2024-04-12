e3197971ed141f848c9490c047482eb1868bf60e
Ayush <68861149+adesai1000@users.noreply.github.com>
2024-04-12 13:32:05 +0530

    UI Fixes

--- client/src/Pages/Messenger/Messenger.jsx
+++ client/src/Pages/Messenger/Messenger.jsx
@@ -2,142 +2,169 @@ import React, { useEffect, useRef, useState } from "react";
 import { useCookies } from "react-cookie";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
-import Navbar from '../../components/Navbar';
-import { ActiveConv } from '../../components/ActiveConv/ActiveConv';
-import { Message } from '../../components/message/Message';
+import Navbar from "../../components/Navbar";
+import { ActiveConv } from "../../components/ActiveConv/ActiveConv";
+import { Message } from "../../components/message/Message";
 import { RiMessageLine } from "react-icons/ri";
-import { FaRegStar } from 'react-icons/fa';
-import './Messenger.scss';
+import { FaRegStar } from "react-icons/fa";
+import "./Messenger.scss";
 
 export default function Messenger() {
-    const navigate = useNavigate();
-    const [cookies, removeCookie] = useCookies([]);
-    const [username, setUsername] = useState("");
-    const [id, setId] = useState("");
-    const [conversations, setConversations] = useState([]);
-    const [currentChat, setCurrentChat] = useState(null);
-    const [messages, setMessages] = useState([]);
-    const [newMessage, setNewMessage] = useState("");
-    const scrollRef = useRef();
+  const navigate = useNavigate();
+  const [cookies, removeCookie] = useCookies([]);
+  const [username, setUsername] = useState("");
+  const [id, setId] = useState("");
+  const [conversations, setConversations] = useState([]);
+  const [currentChat, setCurrentChat] = useState(null);
+  const [messages, setMessages] = useState([]);
+  const [newMessage, setNewMessage] = useState("");
+  const scrollRef = useRef();
 
-    useEffect(() => {
-        const verifyCookie = async () => {
-            if (!cookies.token) {   
-                navigate("/login");
-            }
-            try {
-                const { data } = await axios.post(
-                    "http://localhost:5000",
-                    {},
-                    { withCredentials: true }
-                );
-                const { user } = data;
-                setUsername(user.username);
-                setId(user._id);
-            } catch (error) {
-                console.error(error);
-                removeCookie("token");
-                navigate("/login");
-            }
-        };
-        verifyCookie();
-    }, [cookies, navigate, removeCookie]);
-
-    useEffect(() => {
-        const getChats = async () => {
-            try {
-                const { data } = await userChats(username);
-                setConversations(data);
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        if (username) {
-            getChats();
-        }
-    }, [username]);
-
-    useEffect(() => {
-        const getMessages = async () => {
-            try {
-                const res = await axios.get(`http://localhost:5000/message/${currentChat?._id}`);
-                setMessages(res.data);
-                scrollRef.current?.scrollIntoView({ behavior: "smooth" });
-            } catch (error) {
-                console.log(error);
-            }
-        };
-        if (currentChat) {
-            const intervalId = setInterval(getMessages, 1000);
-            scrollRef.current?.scrollIntoView({ behavior: "smooth" });
-            return () => clearInterval(intervalId);
-        }
-    }, [currentChat]);
-
-    const userChats = async () => {
-        const API = axios.create({ baseURL: 'http://localhost:5000' });
-        return API.get(`/chat/${id}`);
+  useEffect(() => {
+    const verifyCookie = async () => {
+      if (!cookies.token) {
+        navigate("/login");
+      }
+      try {
+        const { data } = await axios.post(
+          "http://localhost:5000",
+          {},
+          { withCredentials: true }
+        );
+        const { user } = data;
+        setUsername(user.username);
+        setId(user._id);
+      } catch (error) {
+        console.error(error);
+        removeCookie("token");
+        navigate("/login");
+      }
     };
+    verifyCookie();
+  }, [cookies, navigate, removeCookie]);
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const message = {
-            chatId: currentChat._id,
-            senderId: id,
-            text: newMessage
-        };
-        try {
-            await axios.post(`http://localhost:5000/message`, message);
-            setNewMessage("");
-        } catch (error) {
-            console.log(error);
-        }
+  useEffect(() => {
+    const getChats = async () => {
+      try {
+        const { data } = await userChats(username);
+        setConversations(data);
+      } catch (error) {
+        console.log(error);
+      }
     };
+    if (username) {
+      getChats();
+    }
+  }, [username]);
 
-    return (
-        <>
-            <Navbar username={username} />
-            <div className="messenger">
-                <div className='chatMenu'>
-                    <div className="chatMenuWrapper">
-                        <div className='convoHeading'>Convoz</div>
-                        {conversations.map((conversation) => (
-                            <div key={conversation._id} onClick={() => setCurrentChat(conversation)}>
-                                <ActiveConv conversation={conversation} currentUser={id} />
-                            </div>
-                        ))}
-                    </div>
+  useEffect(() => {
+    const getMessages = async () => {
+      try {
+        const res = await axios.get(
+          `http://localhost:5000/message/${currentChat?._id}`
+        );
+        setMessages(res.data);
+        scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    if (currentChat) {
+      const intervalId = setInterval(getMessages, 1000);
+      scrollRef.current?.scrollIntoView({ behavior: "smooth" });
+      return () => clearInterval(intervalId);
+    }
+  }, [currentChat]);
+
+  const userChats = async () => {
+    const API = axios.create({ baseURL: "http://localhost:5000" });
+    return API.get(`/chat/${id}`);
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const message = {
+      chatId: currentChat._id,
+      senderId: id,
+      text: newMessage,
+    };
+    try {
+      await axios.post(`http://localhost:5000/message`, message);
+      setNewMessage("");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <>
+      <Navbar username={username} />
+      <div className="messenger">
+        <div className="chatMenu">
+          <div className="chatMenuWrapper">
+            <div className="convoHeading">Convoz</div>
+            {conversations.map((conversation) => (
+              <div
+                key={conversation._id}
+                onClick={() => setCurrentChat(conversation)}
+              >
+                <ActiveConv conversation={conversation} currentUser={id} />
+              </div>
+            ))}
+          </div>
+        </div>
+        <div className="chatBox">
+          <div className="chatBoxWrapper">
+            {currentChat ? (
+              <>
+                <div className="messageHeading">
+                  {id === currentChat.members[0]
+                    ? currentChat.members[3]
+                    : currentChat.members[2]}
                 </div>
-                <div className='chatBox'>
-                    <div className="chatBoxWrapper">
-                        {currentChat ?
-                            <>
-                                <div className='messageHeading'>{id === currentChat.members[0] ? currentChat.members[3] : currentChat.members[2]}</div>
-                                <div className="chatBoxTop">
-                                    {messages.map(m => (
-                                        <div key={m._id} ref={scrollRef}>
-                                            <Message message={m} own={m.senderId === id} />
-                                        </div>
-                                    ))}
-                                </div>
-                                <div className="chatBoxBottom">
-                                    <textarea className='chatMessageInput' placeholder='Message'
-                                        onChange={(e) => setNewMessage(e.target.value)}
-                                        value={newMessage}
-                                    ></textarea>
-                                    <button className='chatSubmitButton' onClick={handleSubmit}>Send</button>
-                                </div>
-                            </> :
-                            <>
-                                <div className='defaultDisplay'>
-                                    <RiMessageLine size="5rem" />
-                                    <div className="defaultDisplayHeading">Convoz Messenger</div>
-                                    <div className='defaultDisplayText flex items-center gap-1'><FaRegStar className='mr-1 text-yellow-500' />Don't forget to star the <a href="https://github.com/adesai1000/Convoz" target="_blank" rel="noopener noreferrer" className="text-[#1976D2] underline font-semibold"> Repo!</a></div>
-                                </div>
-                            </>}
+                <div className="chatBoxTop">
+                  {messages.map((m) => (
+                    <div key={m._id} ref={scrollRef}>
+                      <Message message={m} own={m.senderId === id} />
                     </div>
+                  ))}
                 </div>
-            </div>
-        </>
-    )
+                <div className="chatBoxBottom">
+                  <textarea
+                    className="chatMessageInput"
+                    placeholder="Message"
+                    onChange={(e) => setNewMessage(e.target.value)}
+                    value={newMessage}
+                  ></textarea>
+                  <button className="chatSubmitButton" onClick={handleSubmit}>
+                    Send
+                  </button>
+                </div>
+              </>
+            ) : (
+              <>
+                <div className="defaultDisplay">
+                  <RiMessageLine size="5rem" />
+                  <div className="defaultDisplayHeading">Convoz Messenger</div>
+                  <div className="defaultDisplayText flex items-center gap-1">
+                    <FaRegStar className="mr-1 text-yellow-500" />
+                    Don't forget to star the{" "}
+                    <a
+                      href="https://github.com/adesai1000/Convoz"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-[#1976D2] underline font-semibold"
+                    >
+                      {" "}
+                      Repo!
+                    </a>
+                  </div>
+                </div>
+              </>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- client/src/Pages/Vip.jsx
+++ client/src/Pages/Vip.jsx
@@ -3,9 +3,9 @@ import { useNavigate } from "react-router-dom";
 import { useCookies } from "react-cookie";
 import axios from "axios";
 import Navbar from "../components/Navbar";
-import { MdOutlineVerified } from "react-icons/md";
+import { MdOutlineVerified } from "react-icons/md"; 
 import { FaDollarSign } from "react-icons/fa";
-
+import { RiVipCrownLine } from "react-icons/ri";
 const Vip = () => {
   const navigate = useNavigate();
   const [cookies, removeCookie] = useCookies([]);
@@ -45,14 +45,14 @@ const Vip = () => {
   return (
     <>
       <Navbar username={username} />
-      <div className="mt-10 bg-black flex justify-center items-center">
-        <div className="bg-[#093726] h-auto w-96 rounded-lg shadow-lg p-2">
+      <div className="mt-8 bg-black flex justify-center items-center">
+        <div className="bg-black h-auto w-96 rounded-lg shadow-lg p-2 border-2 border-slate-600">
           <MdOutlineVerified className="text-5xl text-[#eab308] mt-2 mb-1 ml-4" />
           <h1 className="text-4xl font-bold font-poppins text-white ml-4">
             Become a VIP
           </h1>
           <div className="flex flex-row items-center justify-center mt-2">
-          <FaDollarSign className="text-[#D4AF37] text-5xl mr-4" />
+          <RiVipCrownLine  className="text-[#D4AF37] text-4xl mr-4" />
             <div className="mt-2 h-20 w-52 mb-2 bg-white p-2 rounded-3xl border-4 border-[#eab308] shadow-md">
               <div className="flex flex-row items-center">
                 <img
@@ -64,7 +64,7 @@ const Vip = () => {
                 <MdOutlineVerified className="text-4xl text-[#eab308] mt-2 mb-1" />
               </div>
             </div>
-            <FaDollarSign className="text-[#D4AF37] text-5xl ml-4" />
+            <RiVipCrownLine  className="text-[#D4AF37] text-4xl ml-4" />
           </div>
           <div className="text-white flex mt-5 flex-col">
             <div className="flex flex-row mb-2 ml-3">
@@ -85,7 +85,7 @@ const Vip = () => {
                 </p>
               </div>
             </div>
-            <button className="bg-[#c5a333] hover:bg-[#D4AF37] text-white py-3 px-6  rounded-xl text-2xl font-bold shadow-xl">Pay $5</button>
+            <button className="bg-[#1976D2] hover:bg-[#D4AF37] text-white py-3 px-6  rounded-xl text-2xl font-bold shadow-xl">Pay $5</button>
           </div>
         </div>
       </div>
